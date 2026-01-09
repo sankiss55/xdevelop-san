f1dedd4f9f654d8616b0da8291b4dc15295eb35a
sankiss55 <sanchezvera490@gmail.com>
2026-01-09 02:22:56 +0000

    Correccion error jwt

--- app/lib/jwt.ts
+++ app/lib/jwt.ts
@@ -1,7 +1,7 @@
 //validación de JWT **requerimiento**
 import jwt from 'jsonwebtoken';
 
-const JWT_SECRET = process.env.JWT_KEY_SECRET;
+const JWT_SECRET = process.env.JWT_KEY_SECRET as string;
 
 export interface TokenJWT {
   matricula: string;
@@ -11,12 +11,19 @@ export interface TokenJWT {
 }
 //crea un token JWT con la llave secreta y poniendo un tiempo de expiración de 3dias 
 export function generateToken(payload: TokenJWT): string {
+  if (!JWT_SECRET) {
+    throw new Error('JWT_KEY_SECRET no está definida en las variables de entorno');
+  }
+
   return jwt.sign(payload, JWT_SECRET, {
     expiresIn: '3d' 
   });
 }
 //verificacin del token JWT 
 export function verifyToken(token: string): TokenJWT | boolean | null {
+  if (!JWT_SECRET) {
+    return false;
+  }
   try {
     return jwt.verify(token, JWT_SECRET) as TokenJWT;
   } catch (error) {
